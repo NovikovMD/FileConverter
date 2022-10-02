3ee5cd4af1d6dee0092e1b769db64ba160fc81de
NovikovMD <matvey393f@mail.ru>
2022-10-02 13:25:42 +0300

    Lets start from the beggining
    /*I've just understood how to do better, so i start from the beggining

--- src/main/java/FileConverter/XML_to_JSON/XML_to_JSON.java
+++ src/main/java/FileConverter/XML_to_JSON/XML_to_JSON.java
@@ -1,24 +1,9 @@
 package FileConverter.XML_to_JSON;
 
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-
 public class XML_to_JSON {
 
-    public static Document parseXML(String path) throws ParserConfigurationException, IOException, SAXException {
-        // Make builder which can parse XML file
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
+    public static void parseXML(String path) {
 
-        //Return parsed document
-        return builder.parse(new File(path));
     }
 
 }
