1df5b73ca76a3a6c58154e955e4901a6a6920c59
NovikovMD <matvey393f@mail.ru>
2022-10-15 16:09:40 +0300

    New commit
    /*Added convert JSON to XML method and test

--- src/main/java/FileConverter/JSON_to_XML/JSON_to_XML.java
+++ src/main/java/FileConverter/JSON_to_XML/JSON_to_XML.java
@@ -1,16 +1,22 @@
 package FileConverter.JSON_to_XML;
 
 import FileConverter.Classes.JSON.JSON;
+import FileConverter.Classes.JSON.JSONdevStudio;
+import FileConverter.Classes.JSON.JSONgame;
+import FileConverter.Classes.XML.*;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class JSON_to_XML {
 
-    public static JSON parseXML(String path) throws IOException {
+    private static JSON games = new JSON();
+
+    public static JSON parseJSON(String path) throws IOException {
         File fl = new File(path);
         JsonFactory factory = new JsonFactory();
         JsonParser parser = factory.createParser(fl);
@@ -18,7 +24,6 @@ public class JSON_to_XML {
         //point to
         parser.nextToken();
         parser.nextToken();
-        JSON jsonOutput = new JSON();
 
 
         if (parser.nextToken() == JsonToken.START_ARRAY) {
@@ -26,65 +31,147 @@ public class JSON_to_XML {
             while (parser.nextToken() != JsonToken.END_ARRAY) {
 
                 //checker if we're looking at "{" / "}"
-                if (parser.getCurrentName()==null)
+                if (parser.getCurrentName() == null)
                     continue;
                 String key = parser.getCurrentName();
 
-                if(key.equals("name"))
-                {
-                    jsonOutput.addGame("place_holder",-1,"place_holder");
+                if (key.equals("name")) {
+                    games.addGame("place_holder", -1, "place_holder");
                     parser.nextToken();
-                    jsonOutput.getLastGame().setName(parser.getText());
-                }
-                else if (key.equals("year")){
+                    games.getLastGame().setName(parser.getText());
+                } else if (key.equals("year")) {
                     parser.nextToken();
-                    jsonOutput.getLastGame().setYear(Integer.parseInt(parser.getText()));
-                }
-                else if (key.equals("gamePublisher")){
+                    games.getLastGame().setYear(Integer.parseInt(parser.getText()));
+                } else if (key.equals("gamePublisher")) {
                     parser.nextToken();
-                    jsonOutput.getLastGame().setGamePublisher(parser.getText());
-                }
-                else if (key.equals("platforms")) {
+                    games.getLastGame().setGamePublisher(parser.getText());
+                } else if (key.equals("platforms")) {
                     parser.nextToken();
-                    while (parser.nextToken() != JsonToken.END_ARRAY){
-                        jsonOutput.getLastGame().addPlatform(parser.getText());
+                    while (parser.nextToken() != JsonToken.END_ARRAY) {
+                        parser.nextToken();
+                        String some = parser.getText();
+                        if (parser.getCurrentName().equals("name")) {
+                            parser.nextToken();
+                            some = parser.getText();
+                            games.getLastGame().addPlatform(parser.getText());
+                            parser.nextToken();
+                        }
                     }
-                }
-                else if (key.equals("developer_studios")) {
+                } else if (key.equals("developer_studios")) {
                     parser.nextToken();
                     parser.nextToken();
 
                     //loop until end of devStudios
-                    while (parser.nextToken() != JsonToken.END_ARRAY){
+                    while (parser.nextToken() != JsonToken.END_ARRAY) {
 
                         //checker if we're looking at "{" / "}"
-                        if (parser.getCurrentName()==null)
+                        if (parser.getCurrentName() == null)
                             continue;
-                        if(parser.getCurrentName().equals("name")) {
+                        if (parser.getCurrentName().equals("name")) {
                             parser.nextToken();
-                            jsonOutput.getLastGame().addDevStudio(parser.getText(),-1,"place_holder");
-                        }
-                        else if(parser.getCurrentName().equals("yearOfFoundation")) {
+                            games.getLastGame().addDevStudio(parser.getText(), -1, "place_holder");
+                        } else if (parser.getCurrentName().equals("yearOfFoundation")) {
                             parser.nextToken();
-                            jsonOutput.getLastGame().getLastDevStudio().setYearOfFoundation(Integer.parseInt(parser.getText()));
-                        }
-                        else if(parser.getCurrentName().equals("url")) {
+                            games.getLastGame().getLastDevStudio().setYearOfFoundation(Integer.parseInt(parser.getText()));
+                        } else if (parser.getCurrentName().equals("url")) {
                             parser.nextToken();
-                            jsonOutput.getLastGame().getLastDevStudio().setURL(parser.getText());
+                            games.getLastGame().getLastDevStudio().setURL(parser.getText());
                         }
                     }
                 }
 
             }
-        }
-        else
+        } else
             return null;
 
 
         //Getting the token name
 
 
-        return jsonOutput;
+        return games;
+    }
+
+    public static XML convert() {
+        XML gameIndustry = new XML();
+
+        //getting started
+        gameIndustry.addPublisher(games.getGames().get(0).getGamePublisher());
+
+
+        for (int i=0;i<games.returnLength();i++){
+            //get current game in json
+            JSONgame jsonGame = games.getGames().get(i);
+
+            //check if we need to create new publisher
+            if (!checkIfPublExists(jsonGame.getGamePublisher(),gameIndustry.getPublishers()))
+                gameIndustry.addPublisher(jsonGame.getGamePublisher());
+
+            XMLgamePublisher XMLpublisher = findPubl(jsonGame.getGamePublisher(),gameIndustry.getPublishers());
+
+            //collect platforms
+            ArrayList<String> XMLplatforms = new ArrayList<>();
+            for (int j=0;j<jsonGame.getPlatforms().size();j++){
+                XMLplatforms.add(jsonGame.getPlatforms().get(j).getName());
+            }
+
+            for (int j=0;j<jsonGame.getDevStudios().size();j++){
+
+                //get current devStudio in json
+                JSONdevStudio jsonDevStudio = jsonGame.getDevStudios().get(j);
+
+                XMLdevStudio XMLdev;
+                //check if we need to create new dev
+                if (!checkIfDevExists(jsonDevStudio.getName(), XMLpublisher.getDevStudios())){
+                    //if it doesn't exist
+                    //update gamePublisher to avoid "place_holder"-----------------------------
+                    XMLpublisher.setName(jsonGame.getGamePublisher());
+
+                    //create new dev
+                    XMLpublisher.addDevStudio(jsonDevStudio.getName(),jsonDevStudio.getYearOfFoundation(),jsonDevStudio.getURL());
+                }
+                XMLdev = findDev(jsonDevStudio.getName(), XMLpublisher.getDevStudios());
+
+                //add game
+                XMLdev.addGame(jsonGame.getName(), jsonGame.getYear());
+
+                //set all platforms
+                XMLgame XMLgame = XMLdev.getGames().get(XMLdev.getLength()-1);
+                for (String xmlPlatform : XMLplatforms) {
+                    XMLgame.addPlatform(xmlPlatform);
+                }
+            }
+        }
+
+        return gameIndustry;
+    }
+
+    private static boolean checkIfDevExists(String name, ArrayList<XMLdevStudio> devs){
+        for (XMLdevStudio dev : devs) {
+            if (dev.getName().equals(name))
+                return true;
+        }
+        return false;
+    }
+    private static XMLdevStudio findDev(String name, ArrayList<XMLdevStudio> devs){
+        for (int i=devs.size()-1;i>=0;i--){
+            if (devs.get(i).getName().equals(name))
+                return devs.get(i);
+        }
+        return null;
+    }
+    private static boolean checkIfPublExists(String name, ArrayList<XMLgamePublisher> publs){
+        for (XMLgamePublisher publ : publs) {
+            if (publ.getName().equals(name))
+                return true;
+        }
+        return false;
+    }
+    private static XMLgamePublisher findPubl(String name, ArrayList<XMLgamePublisher> publs){
+        for (int i=publs.size()-1;i>=0;i--){
+            if (publs.get(i).getName().equals(name))
+                return publs.get(i);
+        }
+        return null;
     }
 
 }
--- src/test/java/TestJSONtoXML.java
+++ src/test/java/TestJSONtoXML.java
@@ -1,22 +1,19 @@
 import FileConverter.Classes.JSON.JSON;
 import FileConverter.Classes.JSON.JSONgame;
-import FileConverter.Classes.JSON.JSONplatform;
-import FileConverter.Classes.XML.XMLgamePublisher;
+import FileConverter.Classes.XML.*;
 import FileConverter.JSON_to_XML.JSON_to_XML;
-import FileConverter.XML_to_JSON.XML_to_JSON;
 import org.junit.Assert;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class TestJSONtoXML {
     @Test
     public void tryParse1lvl() throws IOException {
         JSONgame game = new JSONgame("The Warriors", 2005, "Rockstar");
-        JSON some = JSON_to_XML.parseXML("src/test/resources/TestInput.json");
+        JSON some = JSON_to_XML.parseJSON("src/test/resources/TestInput.json");
         assert some != null;
         JSONgame oneGame = some.getGames().get(0);
         Assert.assertEquals(oneGame.getName(), game.getName());
@@ -26,7 +23,7 @@ public class TestJSONtoXML {
     @Test
     public void tryParseSecondObject() throws IOException {
         JSONgame game = new JSONgame("Manhunt 2", 2007, "Rockstar");
-        JSON some = JSON_to_XML.parseXML("src/test/resources/TestInput.json");
+        JSON some = JSON_to_XML.parseJSON("src/test/resources/TestInput.json");
         assert some != null;
         JSONgame oneGame = some.getGames().get(1);
         Assert.assertEquals(oneGame.getName(), game.getName());
@@ -35,7 +32,7 @@ public class TestJSONtoXML {
     }
     @Test
     public void tryParseAll() throws IOException {
-        JSON some = JSON_to_XML.parseXML("src/test/resources/TestInput.json");
+        JSON some = JSON_to_XML.parseJSON("src/test/resources/TestInput.json");
         JSONgame oneGame = some.getGames().get(0);
 
         Assert.assertEquals(oneGame.getPlatforms().get(0).getName(), "PlayStation 2");
@@ -57,6 +54,31 @@ public class TestJSONtoXML {
         Assert.assertEquals(oneGame.getDevStudios().get(0).getName(), "Rockstar toronto");
         Assert.assertEquals(oneGame.getDevStudios().get(0).getYearOfFoundation(), 1981);
         Assert.assertEquals(oneGame.getDevStudios().get(0).getURL(), "www.rockstartoronto.com");
+
+    }
+
+    @Test
+    public void tryConvertXMLtoJSONAll() throws IOException {
+        JSON_to_XML.parseJSON("src/test/resources/TestInput.json");
+
+        XML compare = JSON_to_XML.convert();
+        XMLgamePublisher gamePublisher = compare.getPublishers().get(0);
+        XMLdevStudio dev = gamePublisher.getDevStudios().get(0);
+        XMLgame game = dev.getGames().get(0);
+        ArrayList<XMLplatform> platforms = game.getPlatforms();
+
+        Assert.assertEquals(gamePublisher.getName(),"Rockstar");
+
+        Assert.assertEquals(dev.getName(),"Rockstar Toronto");
+        Assert.assertEquals(dev.getYearOfFoundation(),1981);
+        Assert.assertEquals(dev.getURL(),"www.rockstartoronto.com");
+
+        Assert.assertEquals(game.getName(),"The Warriors");
+        Assert.assertEquals(game.getYear(),2005);
+
+        Assert.assertEquals(platforms.get(0).getName(),"PlayStation 2");
+        Assert.assertEquals(platforms.get(1).getName(),"PlayStation Portable");
+        Assert.assertEquals(platforms.get(2).getName(),"XBox");
 
     }
 }
