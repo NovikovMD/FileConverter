51923c76d2b167ccf9a73de427e4e745e9e61a78
NovikovMD <matvey393f@mail.ru>
2022-11-29 17:38:37 +0300

    Добавил логгер для FileConverter

--- src/main/java/file_converter/FileConverter.java
+++ src/main/java/file_converter/FileConverter.java
@@ -4,6 +4,7 @@ import file_converter.classes.json.JsonUpperClass;
 import file_converter.classes.xml.XmlUpperClass;
 import file_converter.json_to_xml.JsonToXml;
 import file_converter.xml_to_json.XmlToJson;
+import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,6 +18,7 @@ import java.util.Scanner;
  * Обработчик входных данных.
  */
 public class FileConverter {
+    private static final Logger LOGGER = Logger.getLogger(FileConverter.class);
     private static final JsonToXml JSON_TO_XML = new JsonToXml();
     private static final XmlToJson XML_TO_JSON = new XmlToJson();
 
@@ -29,15 +31,15 @@ public class FileConverter {
      * @throws Exception если в ходе работы произошли любые ошибки
      */
     public void doParse(List<String> params) throws Exception {
-        //Logger.getInstance().info("Начало работы программы");
+        LOGGER.info("Начало работы программы");
 
         if (params == null) {
-            //Logger.getInstance().error("Некорректный ввод данных. Завершение программы.");
+            LOGGER.error("Некорректный ввод данных. Завершение программы.");
             throw new Exception("Некорректный ввод данных.");
         }
 
         if (params.size() == 1) {
-            //Logger.getInstance().error("Некорректный ввод данных. Завершение программы.");
+            LOGGER.error("Некорректный ввод данных. Завершение программы.");
             throw new Exception("Некорректный ввод данных.");
         }
         String path;
@@ -45,7 +47,7 @@ public class FileConverter {
 
 
         if (params.size() == 0) {
-            //Logger.getInstance().warn("Нет входных данных. Попытка запроса у пользователя.");
+            LOGGER.warn("Нет входных данных. Попытка запроса у пользователя.");
 
             Scanner inp = new Scanner(System.in);
             System.out.print("Введите абсолютный путь к файлу: ");
@@ -67,11 +69,11 @@ public class FileConverter {
         } else if (firstExtension.equals("xml") && secondExtension.equals("json")) {
             parseXml(path, newPath);
         } else {
-            //Logger.getInstance().error("Некорректный формат входных данных. Завершение программы.");
+            LOGGER.error("Некорректный формат входных данных. Завершение программы.");
             throw new Exception("Некорректный формат входных данных.");
         }
 
-        //Logger.getInstance().info("Успешное завершение работы программы");
+        LOGGER.info("Успешное завершение работы программы");
     }
 
     private String getExtension(final String newPath) {
@@ -80,31 +82,31 @@ public class FileConverter {
     }
 
     private void parseXml(final String path, final String newPath) throws Exception {
-        //Logger.getInstance().info("Начало работы парсинга XML");
+        LOGGER.info("Начало работы парсинга XML");
 
         final XmlUpperClass parsedClass;
         try {
             parsedClass = XML_TO_JSON.parseXml(path);
         } catch (ParserConfigurationException | SAXException | IOException exception) {
-            //Logger.getInstance().error("Не удалось считать файл XML.", exception);
+            LOGGER.error("Не удалось считать файл XML.", exception);
             throw new Exception("Не удалось считать файл XML.", exception);
         } catch (IllegalArgumentException exception) {
-            //Logger.getInstance().error("Неверный путь к XML файлу.", exception);
+            LOGGER.error("Неверный путь к XML файлу.", exception);
             throw new Exception("Неверный путь к XML файлу.", exception);
         }
-        //Logger.getInstance().info("Успешное завершение парсинга XML.");
+        LOGGER.info("Успешное завершение парсинга XML.");
 
         try {
             XML_TO_JSON.createJson(XML_TO_JSON.convert(parsedClass), newPath);
         } catch (IOException ioException) {
-            //Logger.getInstance().error("Не удалось создать JSON файл.", ioException);
+            LOGGER.error("Не удалось создать JSON файл.", ioException);
             throw new Exception("Не удалось создать JSON файл.", ioException);
         } catch (IllegalArgumentException exception) {
-            //Logger.getInstance().fatal("Не удалось конвертировать XML классы в JSON.", exception);
+            LOGGER.fatal("Не удалось конвертировать XML классы в JSON.", exception);
             throw new Exception("Не удалось конвертировать XML классы в JSON.", exception);
         }
 
-        //Logger.getInstance().info("Успешно создан JSON файл.");
+        LOGGER.info("Успешно создан JSON файл.");
     }
 
     private void parseJson(final String path, final String newPath) throws Exception {
@@ -112,23 +114,23 @@ public class FileConverter {
         try {
             jsonClass = JSON_TO_XML.parseJson(path);
         } catch (IOException exception) {
-            //Logger.getInstance().error("Не удалось считать файл JSON.", exception);
+            LOGGER.error("Не удалось считать файл JSON.", exception);
             throw new Exception("Не удалось считать файл JSON.", exception);
         } catch (IllegalArgumentException exception) {
-            //Logger.getInstance().error("Неверный путь к JSON файлу.", exception);
+            LOGGER.error("Неверный путь к JSON файлу.", exception);
             throw new Exception("Неверный путь к JSON файлу.", exception);
         }
 
         try {
             JSON_TO_XML.createXML(JSON_TO_XML.convert(jsonClass), newPath);
         } catch (FileNotFoundException exception) {
-            //Logger.getInstance().error("Введен неверный путь к файлу XML.", exception);
+            LOGGER.error("Введен неверный путь к файлу XML.", exception);
             throw new Exception("Введен неверный путь к файлу XML.", exception);
         } catch (XMLStreamException exception) {
-            //Logger.getInstance().error("Не удалось создать XML файл.", exception);
+            LOGGER.error("Не удалось создать XML файл.", exception);
             throw new Exception("Не удалось создать XML файл.", exception);
         } catch (IllegalArgumentException exception) {
-            //Logger.getInstance().error("Не удалось конвертировать JSON классы в XML.", exception);
+            LOGGER.error("Не удалось конвертировать JSON классы в XML.", exception);
             throw new Exception("Не удалось конвертировать JSON классы в XML.", exception);
         }
     }
